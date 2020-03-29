6c5f9c748b278e7e06e59004eebf4f3315d29e30
devnido <luis.nido@hotmail.com>
2020-03-29 04:39:22 -0300

    testing user service

--- tests/unit/user.service.test.js
+++ tests/unit/user.service.test.js
@@ -16,6 +16,7 @@ describe('Testing user service', () => {
     beforeEach('Prepare functions', function() {
 
         user = {...userDbMock }
+        user.toObject = () => user
 
         userRepository = {
             existsByEmail: sinon.stub().resolves(true),
@@ -23,7 +24,16 @@ describe('Testing user service', () => {
             existsByValidResetPassToken: sinon.stub().resolves(true),
             getById: sinon.stub().withArgs("5e376c66ce68605aa0ed1149").resolves(user),
             getByEmail: sinon.stub().withArgs("user@email.com").resolves(user),
-            insert: sinon.stub().returns(user)
+            getByIdWithRefreshToken: sinon.stub().withArgs("asdasdasdasdasdadada").resolves(user),
+            getByResetPassToken: sinon.stub().withArgs("asdasdasdasdasdadada").resolves(user),
+            insert: sinon.stub().returns(user),
+            getToAuthenticate: sinon.stub().withArgs("user@email.com").resolves(user),
+            setRefreshToken: sinon.stub().resolves({ n: 1, nModified: 1, ok: 1 }),
+            setResetPassToken: sinon.stub().resolves({ n: 1, nModified: 1, ok: 1 }),
+            setNewPassword: sinon.stub().resolves({ n: 1, nModified: 1, ok: 1 }),
+            removeResetPassToken: sinon.stub().resolves({ n: 1, nModified: 1, ok: 1 }),
+            removeRefreshToken: sinon.stub().resolves({ n: 1, nModified: 1, ok: 1 }),
+            delete: sinon.stub().resolves({ n: 1, deletedCount: 1, ok: 1 }),
         }
 
         passwordService = {
@@ -36,13 +46,6 @@ describe('Testing user service', () => {
 
     })
 
-
-
-
-
-
-
-
     it('Should register a user', function(done) {
 
         service.register(user.email, user.name, user.password)
@@ -52,16 +55,227 @@ describe('Testing user service', () => {
 
                 expect(userAdded).to.be.include({ password: ':)' })
 
+                done()
+            })
+            .catch(e => {
+                done(e)
+            })
+    })
+
+    it('Should authenticate a user', function(done) {
+
+        service.authenticate(user.email, user.password)
+            .then(userAuthenticated => {
+
+                expect(userAuthenticated).to.be.include({ _id: user._id })
+
+                expect(userAuthenticated).to.be.include({ status: 'active' })
 
                 done()
             })
             .catch(e => {
                 done(e)
             })
-
-
     })
 
+    it('Should return true if user exists by email', function(done) {
+        service.existsByEmail(user.email)
+            .then(result => {
+                expect(result).equals(true)
+                done()
+            })
+            .catch(e => {
+                done(e)
+            })
+    })
+
+    it('Should return true if user exists by id', function(done) {
+        service.existsById(user._id)
+            .then(result => {
+                expect(result).equals(true)
+                done()
+            })
+            .catch(e => {
+                done(e)
+            })
+    })
+
+    it('Should return true if user exists by id', function(done) {
+        service.existsById(user._id)
+            .then(result => {
+                expect(result).equals(true)
+                done()
+            })
+            .catch(e => {
+                done(e)
+            })
+    })
+
+    it('Should return true if user exists by reset pass token', function(done) {
+
+        user.resetPassToken = "ajsdjkasj"
+
+        service.existsByResetPassToken(user.resetPassToken)
+            .then(result => {
+                expect(result).equals(true)
+                done()
+            })
+            .catch(e => {
+                done(e)
+            })
+    })
+
+    it('Should return a user if find by email', function(done) {
+
+        service.getByEmail(user.email)
+            .then(userResult => {
+
+                expect(userResult).to.be.include({ _id: user._id })
+
+                expect(userResult).to.be.include({ status: 'active' })
+
+                done()
+            })
+            .catch(e => {
+                done(e)
+            })
+    })
+
+    it('Should return a user if find by email', function(done) {
+
+        user.resetPassToken = "asdasdasdasdasdadada"
+
+        service.getByResetPassToken(user.resetPassToken)
+            .then(userResult => {
+                expect(userResult).to.be.include({ _id: user._id })
+
+                expect(userResult).to.be.include({ status: 'active' })
+
+                done()
+            })
+            .catch(e => {
+                done(e)
+            })
+    })
+
+    it('Should modify a user setting new reset pass token', function(done) {
+
+        user.resetPassToken = "asdasdasdasdasdadada"
+
+        service.storeResetPassToken(user._id, user.resetPassToken)
+            .then(result => {
+                expect(result).to.be.include({ n: 1, nModified: 1, ok: 1 })
+
+                done()
+            })
+            .catch(e => {
+                done(e)
+            })
+    })
+
+    it('Should modify a user setting new refresh token', function(done) {
+
+        user.refreshToken = "asdasdasdasdasdadada"
+
+        service.storeResetPassToken(user._id, user.refreshToken)
+            .then(result => {
+                expect(result).to.be.include({ n: 1, nModified: 1, ok: 1 })
+
+                done()
+            })
+            .catch(e => {
+                done(e)
+            })
+    })
+
+    it('Should return false if user status is active', function(done) {
+
+        service.isBlocked(user._id)
+            .then(result => {
+
+                expect(result).equals(false)
+
+                done()
+            })
+            .catch(e => {
+                done(e)
+            })
+    })
+
+    it('Should return true if user status is blocked', function(done) {
+
+        user.status = 'blocked'
+
+        userRepository.getById = sinon.stub().withArgs("5e376c66ce68605aa0ed1149").resolves(user)
+
+        service = userService({ userRepository, passwordService })
+
+        service.isBlocked(user._id)
+            .then(result => {
+
+                expect(result).equals(true)
+
+                done()
+            })
+            .catch(e => {
+                done(e)
+            })
+    })
+
+    it('Should return true if user has a refresh token', function(done) {
+
+        refreshToken = 'cYVCc3DLN3YmZ9Ws369m7pRxvF5jFheEdelMOfeI3K6ev0TBHlAnqNGGeoSTHDir9Y5GPfxn2deMT20Mz0uOT85z6UiDMwzPeyapmTI0oMRUAes2tzlrB6Ds7d2AunZP'
+
+        userRepository.getByIdWithRefreshToken = sinon.stub().withArgs("5e376c66ce68605aa0ed1149").resolves(user)
+
+        service = userService({ userRepository, passwordService })
+
+        service.hasRefreshToken(user._id, refreshToken)
+            .then(result => {
+
+                expect(result).equals(true)
+
+                done()
+            })
+            .catch(e => {
+                done(e)
+            })
+    })
+
+    it('Should return false if user has no refresh token', function(done) {
+
+        refreshToken = 'cYVCc3DLN3YmZ9Ws369m7pRxvF5jFheEdelMOfeI3K6ev0TBHlAnqNGGeoSTHDir9Y5GPfxn2deMT20Mz0uOT85z6UiDMwzPeyapmTI0oMRUAes2tzlrB6Ds7d2AunZP'
+
+        user.refreshToken = ''
+
+        userRepository.getByIdWithRefreshToken = sinon.stub().withArgs("5e376c66ce68605aa0ed1149").resolves(user)
+
+        service = userService({ userRepository, passwordService })
+
+        service.hasRefreshToken(user._id, refreshToken)
+            .then(result => {
+
+                expect(result).equals(false)
+
+                done()
+            })
+            .catch(e => {
+                done(e)
+            })
+    })
+
+    it('Should modify a user removing reset pass token', function(done) {
+
+        service.removeResetPassToken(user._id)
+            .then(result => {
+                expect(result).to.be.include({ n: 1, nModified: 1, ok: 1 })
+
+                done()
+            })
+            .catch(e => {
+                done(e)
+            })
+    })
 
     afterEach('Clean functions', function() {
         userRepositoryMock = {}
